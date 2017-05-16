7a25dfe95fbf257d7dedb94e6d03f3ca63bb7922
CoolAmu <amrutgupta719@gmail.com>
2017-05-16 13:12:21 +0530

    first commit

---
+++ resources/views/ourdoctors.blade.php
@@ -0,0 +1,239 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+     <hr>
+
+ <h3 align="center"> Doctor Profile </h3>
+
+<hr>
+
+<section class="container">
+   <div id="about" class="about">
+        <!-- container -->
+        <div class="container">
+            <h3 class="w3layouts-title wow fadeInDown animated" align="left" data-wow-delay=".5s"> Dr. Gupta A. J.</h3>   
+            <div class="about-agileitsrow">
+                <div class="col-md-8 about-left wow left animated" data-wow-delay=".5s">
+                    <div class="col-md-4 col-sm-4 col-xs-4 about-img wow left animated" data-wow-delay=".9s">
+                        <img src="assets2/images/img1.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 col-sm-8 about-w3ls-text wow zoomInRight animated" data-wow-delay=".9s">
+                        <br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum. Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat. Proin ut dui a urna efficitur varius. </p>
+                        <p>Quisque molestie cursus mi et congue consectetur adipiscing elit cras rutrum iaculis enim, Lorem ipsum dolor sit amet, non convallis felis mattis at.</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="col-md-4 about-right w3-agileits wow slideInRight animated" data-wow-delay=".5s">
+                    <h4 align="left">Working Day's </h4>
+                    <ul>
+                      <br>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Donec et nibh massa viverra luctus.</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Lorem ipsum dolor sit consectetur </li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //container -->
+    </div>
+    <!-- //about -->
+</section>
+ 
+  <hr>
+
+<section class="container">
+   <div id="about" class="about">
+        <!-- container -->
+        <div class="container">
+            <h3 class="w3layouts-title wow fadeInDown animated" align="left" data-wow-delay=".5s"> Dr. Gupta A. J.</h3>   
+            <div class="about-agileitsrow">
+                <div class="col-md-8 about-left wow left animated" data-wow-delay=".5s">
+                    <div class="col-md-4 col-sm-4 col-xs-4 about-img wow left animated" data-wow-delay=".9s">
+                        <img src="assets2/images/img1.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 col-sm-8 about-w3ls-text wow zoomInRight animated" data-wow-delay=".9s">
+                        <br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum. Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat. Proin ut dui a urna efficitur varius. </p>
+                        <p>Quisque molestie cursus mi et congue consectetur adipiscing elit cras rutrum iaculis enim, Lorem ipsum dolor sit amet, non convallis felis mattis at.</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="col-md-4 about-right w3-agileits wow slideInRight animated" data-wow-delay=".5s">
+                    <h4 align="left">Working Day's </h4>
+                    <ul>
+                      <br>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Donec et nibh massa viverra luctus.</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Lorem ipsum dolor sit consectetur </li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //container -->
+    </div>
+</section>
+
+  <hr>
+
+<section class="container">
+   <div id="about" class="about">
+        <!-- container -->
+        <div class="container">
+            <h3 class="w3layouts-title wow fadeInDown animated" align="left" data-wow-delay=".5s"> Dr. Gupta A. J.</h3>   
+            <div class="about-agileitsrow">
+                <div class="col-md-8 about-left wow left animated" data-wow-delay=".5s">
+                    <div class="col-md-4 col-sm-4 col-xs-4 about-img wow left animated" data-wow-delay=".9s">
+                        <img src="assets2/images/img1.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 col-sm-8 about-w3ls-text wow zoomInRight animated" data-wow-delay=".9s">
+                        <br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum. Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat. Proin ut dui a urna efficitur varius. </p>
+                        <p>Quisque molestie cursus mi et congue consectetur adipiscing elit cras rutrum iaculis enim, Lorem ipsum dolor sit amet, non convallis felis mattis at.</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="col-md-4 about-right w3-agileits wow slideInRight animated" data-wow-delay=".5s">
+                    <h4 align="left">Working Day's </h4>
+                    <ul>
+                      <br>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Donec et nibh massa viverra luctus.</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Lorem ipsum dolor sit consectetur </li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //container -->
+    </div>
+</section>
+
+<section class="container">
+   <div id="about" class="about">
+        <!-- container -->
+        <div class="container">
+            <h3 class="w3layouts-title wow fadeInDown animated" align="left" data-wow-delay=".5s"> Dr. Gupta A. J.</h3>   
+            <div class="about-agileitsrow">
+                <div class="col-md-8 about-left wow left animated" data-wow-delay=".5s">
+                    <div class="col-md-4 col-sm-4 col-xs-4 about-img wow left animated" data-wow-delay=".9s">
+                        <img src="assets2/images/img1.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 col-sm-8 about-w3ls-text wow zoomInRight animated" data-wow-delay=".9s">
+                        <br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum. Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat. Proin ut dui a urna efficitur varius. </p>
+                        <p>Quisque molestie cursus mi et congue consectetur adipiscing elit cras rutrum iaculis enim, Lorem ipsum dolor sit amet, non convallis felis mattis at.</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="col-md-4 about-right w3-agileits wow slideInRight animated" data-wow-delay=".5s">
+                    <h4 align="left">Working Day's </h4>
+                    <ul>
+                      <br>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Donec et nibh massa viverra luctus.</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Lorem ipsum dolor sit consectetur </li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //container -->
+    </div>
+</section>
+ 
+  <hr>
+
+<section class="container">
+   <div id="about" class="about">
+        <!-- container -->
+        <div class="container">
+            <h3 class="w3layouts-title wow fadeInDown animated" align="left" data-wow-delay=".5s"> Dr. Gupta A. J.</h3>   
+            <div class="about-agileitsrow">
+                <div class="col-md-8 about-left wow left animated" data-wow-delay=".5s">
+                    <div class="col-md-4 col-sm-4 col-xs-4 about-img wow left animated" data-wow-delay=".9s">
+                        <img src="assets2/images/img1.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 col-sm-8 about-w3ls-text wow zoomInRight animated" data-wow-delay=".9s">
+                        <br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum. Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat. Proin ut dui a urna efficitur varius. </p>
+                        <p>Quisque molestie cursus mi et congue consectetur adipiscing elit cras rutrum iaculis enim, Lorem ipsum dolor sit amet, non convallis felis mattis at.</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="col-md-4 about-right w3-agileits wow slideInRight animated" data-wow-delay=".5s">
+                    <h4 align="left">Working Day's </h4>
+                    <ul>
+                      <br>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Donec et nibh massa viverra luctus.</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Lorem ipsum dolor sit consectetur </li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //container -->
+    </div>
+</section>
+
+  <hr>
+
+<section class="container">
+   <div id="about" class="about">
+        <!-- container -->
+        <div class="container">
+            <h3 class="w3layouts-title wow fadeInDown animated" align="left" data-wow-delay=".5s"> Dr. Gupta A. J.</h3>   
+            <div class="about-agileitsrow">
+                <div class="col-md-8 about-left wow left animated" data-wow-delay=".5s">
+                    <div class="col-md-4 col-sm-4 col-xs-4 about-img wow left animated" data-wow-delay=".9s">
+                        <img src="assets2/images/img1.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 col-sm-8 about-w3ls-text wow zoomInRight animated" data-wow-delay=".9s">
+                        <br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum. Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat. Proin ut dui a urna efficitur varius. </p>
+                        <p>Quisque molestie cursus mi et congue consectetur adipiscing elit cras rutrum iaculis enim, Lorem ipsum dolor sit amet, non convallis felis mattis at.</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="col-md-4 about-right w3-agileits wow slideInRight animated" data-wow-delay=".5s">
+                    <h4 align="left">Working Day's </h4>
+                    <ul>
+                      <br>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Donec et nibh massa viverra luctus.</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Lorem ipsum dolor sit consectetur </li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Sed eu augue eget massa gravida</li>
+                        <li><span class="glyphicon glyphicon-menu-right"></span>Vivamus finibus velit quis orci</li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //container -->
+    </div>
+</section>
+</div>
+
+@endsection
